20c4493cfd366844f97475b1c29764b7c6c4e127
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-02-15 10:10:27 -0700

    cc commit

--- src/App.js
+++ src/App.js
@@ -8,27 +8,66 @@ import FamilyChanger from './components/FamilyChanger';
 import TextContainer from './components/TextContainer';
 
 class App extends Component {
-  // constructor
+  constructor(){
+    super();
+    this.state = {
+      fontColor:'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }
+  }
 
-  // updateColor
+updateColor(val){
+  this.setState({
+    fontColor: val
+  })
+}
 
-  // updateSize
+updateSize(val){
+  this.setState({
+    fontSize: val
+  })
+}
 
-  // updateFamily
+updateFamily(val){
+  this.setState({
+    fontFamily: val
+  })
+}
 
-  // updateEditStatus
+updateEditStatus(val){
+  this.setState({
+    allowEdit: val
+  })
+}
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          <EditToggle
+            update={(v) => this.updateEditStatus(v)}>
+          </EditToggle>
+          <ColorChanger
+            update={(v) => this.updateColor(v)}
+            allowEdit={this.state.allowEdit}>
+          </ColorChanger>
+          <SizeChanger
+            update={(v) => this.updateSize(v)}
+            allowEdit={this.state.allowEdit}>
+          </SizeChanger>
+          <FamilyChanger
+            update={(v) => this.updateFamily(v)}
+            allowEdit={this.state.allowEdit}>
+          </FamilyChanger>
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+          <TextContainer
+            fontColor={this.state.fontColor}
+            fontSize={this.state.fontSize}
+            fontFamily={this.state.fontFamily}>
+          </TextContainer>
         </div>
       </div>
     )
